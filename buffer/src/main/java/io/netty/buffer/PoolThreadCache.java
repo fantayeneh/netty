ec5cf8ff55bb1bfe6b652bea9334afe20e62c469
Norman Maurer <nmaurer@redhat.com>
2014-03-01 15:47:03 +0100

    WIP

--- buffer/src/main/java/io/netty/buffer/PoolThreadCache.java
+++ buffer/src/main/java/io/netty/buffer/PoolThreadCache.java
@@ -23,11 +23,60 @@ final class PoolThreadCache {
     final PoolArena<byte[]> heapArena;
     final PoolArena<ByteBuffer> directArena;
 
+    private final PoolChunkCache<byte[]> smallHeapCache;
+    private final PoolChunkCache<ByteBuffer> smallDirectCache;
+
     // TODO: Test if adding padding helps under contention
     //private long pad0, pad1, pad2, pad3, pad4, pad5, pad6, pad7;
 
     PoolThreadCache(PoolArena<byte[]> heapArena, PoolArena<ByteBuffer> directArena) {
         this.heapArena = heapArena;
         this.directArena = directArena;
+        if (heapArena != null) {
+            smallHeapCache = new PoolChunkCache<byte[]>();
+        } else {
+            smallHeapCache = null;
+        }
+        if (directArena != null) {
+            smallDirectCache = new PoolChunkCache<ByteBuffer>();
+        } else {
+            smallDirectCache = null;
+        }
+    }
+
+    final static class PoolChunkCache<T> {
+        private Entry<T> first;
+        private Entry<T> last;
+
+        public void offer(PoolChunk<T> chunk, long handle) {
+            Entry<T> entry = new Entry<T>(chunk, handle);
+            if (first == null) {
+                first = last = entry;
+            } else {
+                last.next = entry;
+            }
+        }
+
+        public Entry<T> poll() {
+            Entry<T> entry = first;
+            if (entry == null) {
+                return null;
+            }
+            first = entry.next;
+            if (first == null) {
+                last = null;
+            }
+            return entry;
+        }
+
+        static final class Entry<T> {
+            private Entry<T> next;
+            final PoolChunk<T> chunk;
+            final long handle;
+            Entry(PoolChunk<T> chunk, long handle) {
+                this.chunk = chunk;
+                this.handle = handle;
+            }
+        }
     }
 }
